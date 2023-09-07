fde07fc3b91e36d23ee778375d5355691ad210c9
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-09-07 21:22:01 +0700

    [modif] detail kursus

--- detail.php
+++ detail.php
@@ -50,8 +50,7 @@
                                 <div class="border-top w-100 mt-3">
                                     <div class="d-flex justify-content-between p-4">
                                         <span class="text-white"><i class="fa fa-user mr-2"></i>Jhon Doe</span>
-                                        <span class="text-white"><i class="fa fa-star mr-2"></i>4.5
-                                            <small>(250)</small></span>
+                                        <span class="text-white"><i class="fa fa-tag mr-2"></i><small>Rp</small> 10000000</span>
                                     </div>
                                 </div>
                             </div>
@@ -64,8 +63,7 @@
                                 <div class="border-top w-100 mt-3">
                                     <div class="d-flex justify-content-between p-4">
                                         <span class="text-white"><i class="fa fa-user mr-2"></i>Jhon Doe</span>
-                                        <span class="text-white"><i class="fa fa-star mr-2"></i>4.5
-                                            <small>(250)</small></span>
+                                        <span class="text-white"><i class="fa fa-tag mr-2"></i><small>Rp</small> 10000000</span>
                                     </div>
                                 </div>
                             </div>
@@ -78,8 +76,7 @@
                                 <div class="border-top w-100 mt-3">
                                     <div class="d-flex justify-content-between p-4">
                                         <span class="text-white"><i class="fa fa-user mr-2"></i>Jhon Doe</span>
-                                        <span class="text-white"><i class="fa fa-star mr-2"></i>4.5
-                                            <small>(250)</small></span>
+                                        <span class="text-white"><i class="fa fa-tag mr-2"></i><small>Rp</small> 10000000</span>
                                     </div>
                                 </div>
                             </div>
@@ -128,12 +125,25 @@
                         </div>
                         <div class="d-flex justify-content-between border-bottom px-4">
                         <h6 class="text-white my-3">Referral code</h6>
-                            <input class="my-3 py-1 px-1" placeholder="Input Referral Code" type="text" p>
+                            <input class="my-3 py-1 px-2" placeholder="Input Referral Code" type="text" p>
                         </div>
+
+                        
+
                         <div class="d-flex justify-content-between px-4">
                         <h6 class="text-white my-3 mr-2"><input type="checkbox"/></h6>
                             <h6 class="text-white my-3">(Opsional) Biaya tambahan sertifikat BNSP* </h6>
-                          
+                        </div>
+
+                        <div class="flex-row justify-content-between px-4">
+                            <div>
+                                 <input  class="text-white my-1 mr-2"  type="checkbox"  name="refund">
+                                <a class="text-white my-3" href="https://softuni.id/terms/Return-and-Refund-Policy_Softuni-Indonesia.pdf" target="_blank" >Return & Refund Policy</a>
+                            </div>
+                            <div class="">
+                                <input type="checkbox"  class="text-white my-1 mr-2"  name="terms">
+                                <a href="https://softuni.id/terms/Terms-and-Conditions_SoftUni-Indonesia-Site.pdf" class="text-white my-3" target="_blank" >Terms & Conditions</a>
+                            </div>
                         </div>
                         <div class="px-4">
                              <a class="btn btn-block btn-warning py-3 px-5" href="">Opsi Cicilan *</a>
@@ -171,40 +181,46 @@
                                 <h6>Java Beginner</h6>
                                 <div class="d-flex">
                                     <small class="text-body mr-3"><i class="fa fa-user text-primary mr-2"></i>Jhon Doe</small>
-                                    <small class="text-body"><i class="fa fa-star text-primary mr-2"></i>4.5 (250)</small>
+                                    <small class="text-body"><i class="fa fa-tag text-primary mr-2"></i>Rp 2000000</small>
                                 </div>
                             </div>
                         </a>
+
                         <a class="d-flex align-items-center text-decoration-none mb-4" href="">
                             <img class="img-fluid rounded" src="img/courses-80x80.jpg" alt="">
                             <div class="pl-3">
-                                <h6>Python Beginner</h6>
+                                <h6>Java Beginner</h6>
                                 <div class="d-flex">
                                     <small class="text-body mr-3"><i class="fa fa-user text-primary mr-2"></i>Jhon Doe</small>
-                                    <small class="text-body"><i class="fa fa-star text-primary mr-2"></i>4.5 (250)</small>
+                                    <small class="text-body"><i class="fa fa-tag text-primary mr-2"></i>Rp 2000000</small>
                                 </div>
                             </div>
                         </a>
+
                         <a class="d-flex align-items-center text-decoration-none mb-4" href="">
                             <img class="img-fluid rounded" src="img/courses-80x80.jpg" alt="">
                             <div class="pl-3">
-                                <h6>Java Webinar</h6>
+                                <h6>Java Beginner</h6>
                                 <div class="d-flex">
                                     <small class="text-body mr-3"><i class="fa fa-user text-primary mr-2"></i>Jhon Doe</small>
-                                    <small class="text-body"><i class="fa fa-star text-primary mr-2"></i>4.5 (250)</small>
+                                    <small class="text-body"><i class="fa fa-tag text-primary mr-2"></i>Rp 2000000</small>
                                 </div>
                             </div>
                         </a>
-                        <a class="d-flex align-items-center text-decoration-none" href="">
+
+                        <a class="d-flex align-items-center text-decoration-none mb-4" href="">
                             <img class="img-fluid rounded" src="img/courses-80x80.jpg" alt="">
                             <div class="pl-3">
-                                <h6>Web design & development courses for beginners</h6>
+                                <h6>Java Beginner</h6>
                                 <div class="d-flex">
                                     <small class="text-body mr-3"><i class="fa fa-user text-primary mr-2"></i>Jhon Doe</small>
-                                    <small class="text-body"><i class="fa fa-star text-primary mr-2"></i>4.5 (250)</small>
+                                    <small class="text-body"><i class="fa fa-tag text-primary mr-2"></i>Rp 2000000</small>
                                 </div>
                             </div>
                         </a>
+                       
+                      
+                       
                     </div>
                 </div>
             </div>
