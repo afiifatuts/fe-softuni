343eaf0e2779f3e7d16ad45d208c652959f26c88
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-09-19 11:55:47 +0700

    [imprv]file structure

--- partnership.php
+++ partnership.php
@@ -80,6 +80,7 @@
                     />
                   </div>
                 </div>
+                
                 <div class="form-group">
                   <input
                     type="text"
